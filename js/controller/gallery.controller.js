18c38a126d676d1fc2dbe120db43000fbe739369
Mor-Meir <123agmor@gmail.com>
2021-11-01 08:11:58 +0200

    gallery

--- js/controller/gallery.controller.js
+++ js/controller/gallery.controller.js
@@ -2,7 +2,7 @@
 
 function renderGallery() {
     var strhtml = gImgs.map(img => {
-        return `<div class="gallery-item" style="background-image: url(imgs/${img.id}.jpg);" onclick="onSelectImg(${img.id})"></div>`
+        return `<div class="gallery-item" style="background-image: url(./imgs/${img.id}.jpg);" onclick="onSelectImg(${img.id})"></div>`
     });
     document.querySelector('.gallery-container').innerHTML = strhtml.join('');
 }
